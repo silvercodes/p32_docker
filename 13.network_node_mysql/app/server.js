260db824af78cf8a593d5b8994f6e8cbb6727931
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2026-02-03 00:00:06 +0300

    add:volumes

---
+++ 13.network_node_mysql/app/server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const mysql = require('mysql2/promise');
+
+const app = express();
+const port = 3000;
+
+const dbconfig = {
+    host: 'mysql-db',           // Использовать имя контейнера с БД
+    user: 'appuser',
+    password: 'userpassword',
+    database: 'test_db',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+};
+
+const pool = mysql.createPool(dbconfig);
+
+app.get('/', async (req, res) => {
+    try {
+        const conn = await pool.getConnection();
+
+        const [rows] = await conn.execute('SELECT * FROM users;');
+
+        conn.release();
+
+        res.json({
+            success: true,
+            data: rows,
+            message: 'Data fetched successfully from MySQL',
+        });
+    } catch (error) {
+        console.log('Database error: ', error);
+
+        res.status(500).json({
+            success: false,
+            error: error.message,
+        });
+    }
+});
+
+app.get('/health', async(req, res) => {
+    try {
+        const conn = await pool.getConnection();
+
+        await conn.ping();
+
+        conn.release();
+
+        res.json({
+            status: 'OK',
+            database: 'connected',
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            database: 'disconnected',
+            error: error.message,
+        });
+    }
+    
+});
+
+app.listen(port, '0.0.0.0', () => {
+    console.log('🟢', `Server started at http://0.0.0.0:${port}`);
+});
