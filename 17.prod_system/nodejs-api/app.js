6863f28778f5a063b7ec813122943e67f20a4c17
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2026-02-12 00:41:03 +0300

    add: nginx

--- 17.prod_system/nodejs-api/app.js
+++ 17.prod_system/nodejs-api/app.js
@@ -25,6 +25,8 @@ const storage = multer.diskStorage({
     }
 });
 
+const upload = multer({storage: storage});
+
 // Настройка БД
 const dbConfig = {
     host: process.env.MYSQL_HOST || 'mysql-db',
@@ -38,6 +40,58 @@ app.get('/', (req, res) => {
     res.json({
         message: 'NodeJS API works!!!',
     });
+});
+
+app.post('/upload', upload.single('file'), async(req, res) => {
+    try {
+        if (! req.file) {
+            return res.status(400).json({error: 'No file uploaded'});
+        }
+
+        const connection = await mysql.createConnection(dbConfig);
+
+        const [result] = await connection.execute(
+            'INSERT INTO files (filename, original_name) VALUES (?, ?)',
+            [req.file.filename, req.file.originalname]
+        );
+
+        await connection.end();
+
+        res.json({
+            message: 'File uploaded successfully',
+            fileId: result.insertId,
+            filename: req.file.filename,
+        });
+    } catch (error) {
+        console.log(`Uploaded error: ${error}`);
+        res.status(500).json({error: 'Uploaded failed'});
+    }
+});
+
+app.get('/files', async (req, res) => {
+    try {
+        const connection = await mysql.createConnection(dbConfig);
+
+        const [rows] = await connection.execute('SELECT * FROM files ORDER BY upload_time DESC');
+
+        await connection.end();
+
+    res.json(rows);
+    } catch (error) {
+        console.log(`Error fetching files: ${error}`);
+        res.status(500).json({error: 'Failed to fetch files list'});
+    }
+});
+
+app.get('/download/:filename', (req, res) => {
+    const filename = req.params.filename;
+    const filePath = path.join('/app/files', filename);
+
+    if (! fs.existsSync(filePath)) {
+        res.status(404).json({error: 'File not found'});
+    }
+
+    res.download(filePath);
 });
 
 
